79fe9009c361dc403a853cbac3fed43134527db3
Prosachi <syosa@cesde.net>
2026-08-14 10:22:52 -0500

    feat: Inclusión de la validación de la licencia de conducción

--- Main.java
+++ Main.java
@@ -4,15 +4,23 @@ public class Main {
     public static void main(String[] args) {
         int EDAD = 18;
         int edadUsuario;
+        String poseerLicencia;
         Scanner leer = new Scanner(System.in);
 
         System.out.println("Digame su edad: ");
         edadUsuario = leer.nextInt();
-        if (edadUsuario >= EDAD) {
-            System.out.println("Es mayor de edad");
+        
+        System.out.println("Tienes licencia de conducción: Si o No ");
+        poseerLicencia = leer.next().toLowerCase();
+
+
+
+        if (edadUsuario >= EDAD && poseerLicencia =="si") {
+            System.out.println("Puede conducir");
         } else {
-            System.out.println("No eres mayor de edad");
+            System.out.println("No puede condicir");
         }
+
         leer.close();
     }
 }
